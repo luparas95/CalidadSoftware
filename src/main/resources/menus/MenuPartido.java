dfa0a9391cefaacfcf73da73190e26b28b89050f
Urarenge <57118342+Urarenge@users.noreply.github.com>
2019-11-18 19:00:46 +0100

    añadido javadoc

--- src/main/resources/menus/MenuPartido.java
+++ src/main/resources/menus/MenuPartido.java
@@ -6,10 +6,18 @@ import main.resources.objects.Partido;
 
 import main.resources.utils.Utils;
 
+/**
+ * Esta clase contiene el menu de la clase Partido y la implementacion de sus metodos
+ * @author: Jose Luis Panadero, Gustavo Adolfo Hernández Quesada, Alvaro Francisco Hernáez Colque
+ */
 public class MenuPartido {
     
     private static final Scanner ENTRADA = new Scanner(System.in);
     
+    /**
+     * Método que imprime en pantalla las opciones del menu principal de la clase Partidos
+     * @author: Jose Luis Panadero, Gustavo Adolfo Hernández Quesada, Alvaro Francisco Hernáez Colque
+     */
     private static void muestraMenuPrincipal() {
     
         System.out.println("------------------------------");
@@ -19,7 +27,12 @@ public class MenuPartido {
         System.out.print("Introduzca la opción deseada: ");
     
     }
-
+    
+    /**
+    * Método que imprime por pantalla los atributos de todos los objetos Partido guardados
+    * @author: Jose Luis Panadero, Gustavo Adolfo Hernández Quesada, Alvaro Francisco Hernáez Colque
+    * @param partidos[] este parametro es un arreglo usado para almacenar todos los objetos Partido
+    */
     private static void muestraPartidos(Partido partidos[]) {
     
         System.out.println("------------------------------");
@@ -39,7 +52,13 @@ public class MenuPartido {
         }
     
     }
-
+    
+    /**
+    * Método que crea un nuevo objeto Partido y lo agrega al arreglo donde son almacenados
+    * @author: Jose Luis Panadero, Gustavo Adolfo Hernández Quesada, Alvaro Francisco Hernáez Colque
+    * @param partidos[] este parametro es un arreglo usado para almacenar todos los objetos Partido
+    * @return Partido[] arreglo usado para almacenar todos los objetos Partido
+    */
     private static Partido[] creaPartido(Partido partidos[]) {
     
         System.out.println("------------------------------");
@@ -54,7 +73,13 @@ public class MenuPartido {
         return Utils.PartidoArrayPush(partido, partidos);
     
     }
-
+    
+    /**
+    * Método que implementa las opciones del menu principal
+    * @author: Jose Luis Panadero, Gustavo Adolfo Hernández Quesada, Alvaro Francisco Hernáez Colque
+    * @param partidos[] este parametro es un arreglo usado para almacenar todos los objetos Partido
+    * @return Partido[] arreglo usado para almacenar todos los objetos Partido 
+    */
     public static Partido[] muestraMenuPartido(Partido partidos[]) {
     
         Boolean noSalir = true;
