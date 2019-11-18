1a30992b60e0d8001dbbd6fb35c27ead3e448e81
Urarenge <57118342+Urarenge@users.noreply.github.com>
2019-11-18 18:06:16 +0100

    añadido javadoc de la clase

--- src/main/resources/objects/Centro.java
+++ src/main/resources/objects/Centro.java
@@ -1,10 +1,21 @@
 package main.resources.objects;
 
+/**
+ * Esta clase define objetos "Centro" cuyos atributos son su nombre (string) y numero de electores (int) del mismo
+ * @author: Jose Luis Panadero, Gustavo Adolfo Hernández Quesada, Alvaro Francisco Hernáez Colque
+ */
 public class Centro {
-
+    
+    //Campos de la clase
     private String nombre;
     private int electores;
     
+    /**
+    * Constructor para un objeto Centro
+    * @author: Jose Luis Panadero, Gustavo Adolfo Hernández Quesada, Alvaro Francisco Hernáez Colque
+    * @param nombre define el nombre del centro
+    * @param electores define el numero de electores del centro 
+    */
     public Centro(String nombre, int electores) {
     
         this.nombre = nombre;
@@ -12,30 +23,54 @@ public class Centro {
     
     }
     
+    /**
+    * Metodo que devuelve el nombre del centro
+    * @author: Jose Luis Panadero, Gustavo Adolfo Hernández Quesada, Alvaro Francisco Hernáez Colque
+    * @return this.nombre es el atributo nombre del centro en cuestion 
+    */
     public String getNombre() {
     
         return this.nombre;
 
     }
     
+    /**
+    * Metodo que devuelve el numero de electores del centro
+    * @author: Jose Luis Panadero, Gustavo Adolfo Hernández Quesada, Alvaro Francisco Hernáez Colque
+    * @return this.electores es el atributo electores del centro en cuestion 
+    */
     public int getElectores() {
     
         return this.electores;
 
     }
     
+    /**
+    * Metodo establece un nuevo nombre para el centro
+    * @author: Jose Luis Panadero, Gustavo Adolfo Hernández Quesada, Alvaro Francisco Hernáez Colque
+    * @param nombre parametro que define el nuevo nombre del centro
+    */
     public void setNombre(String nombre) {
     
         this.nombre = nombre;
 
     }
     
+    /**
+    * Metodo establece una nueva cantidad de electores para el centro
+    * @author: Jose Luis Panadero, Gustavo Adolfo Hernández Quesada, Alvaro Francisco Hernáez Colque
+    * @param electores parametro que define la nueva cantidad de electores del centro
+    */
     public void setElectores(int electores) {
     
         this.electores = electores;
 
     }
     
+    /**
+     * Método que imprime por pantalla los atributos de un objeto Centro
+     * @author: Jose Luis Panadero, Gustavo Adolfo Hernández Quesada, Alvaro Francisco Hernáez Colque
+     */
     public void print() {
     
         System.out.println("------------------------------");
