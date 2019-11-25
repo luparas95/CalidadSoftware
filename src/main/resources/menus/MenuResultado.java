ff0fd1c9de0451bd663f43fc0c73016fe9e936dd
Urarenge <57118342+Urarenge@users.noreply.github.com>
2019-11-25 10:04:07 +0100

    añadido javadoc

--- src/main/resources/menus/MenuResultado.java
+++ src/main/resources/menus/MenuResultado.java
@@ -15,6 +15,12 @@ import main.resources.utils.Utils;
  */
 public class MenuResultado {
     
+    /**
+     * Metodo que imprime por pantalla todos los centros almacenados con un número asociado y devuelve el centro elegido
+     * @author: Jose Luis Panadero, Gustavo Adolfo Hernández Quesada, Alvaro Francisco Hernáez Colque
+     * @param centros[] este parametro es el arreglo usado para almacenar todos los centros del sistema
+     * @return Centro es el centro elegido por el usuario de entre todos los almacenados
+     */
     private static Centro escogeCentro(Centro[] centros) {
     
         Scanner entrada = new Scanner(System.in);
@@ -63,6 +69,12 @@ public class MenuResultado {
     
     }
     
+    /**
+     * Metodo que imprime por pantalla todos los partidos almacenados con un número asociado y devuelve el partido elegido
+     * @author: Jose Luis Panadero, Gustavo Adolfo Hernández Quesada, Alvaro Francisco Hernáez Colque
+     * @param partidos[] este parametro es el arreglo usado para almacenar todos los partidos del sistema
+     * @return Partido es el partido elegido por el usuario de entre todos los almacenados
+     */
     private static Partido escogePartido(Partido[] partidos) {
     
         Scanner entrada = new Scanner(System.in);
@@ -110,7 +122,14 @@ public class MenuResultado {
         return partidoEscogido;
     
     }
-        
+    
+    /**
+     * Metodo que pide introducir al usuario un número de votos válido para el centro escogido
+     * @author: Jose Luis Panadero, Gustavo Adolfo Hernández Quesada, Alvaro Francisco Hernáez Colque
+     * @param resultados[] arreglo usado para almacenar todos los resultados del sistema
+     * @param centro este parametro es el centro escogido por el usuario
+     * @return int es el número de votos de la nueva tripleta
+     */    
     private static int introduceVotos(Resultado[] resultados, Centro centro) {
     
         Scanner entrada = new Scanner(System.in);
@@ -151,6 +170,14 @@ public class MenuResultado {
     
     }
     
+    /**
+     * Metodo que comprueba si ya existe una tripleta con el centro y el partido especificados
+     * @author: Jose Luis Panadero, Gustavo Adolfo Hernández Quesada, Alvaro Francisco Hernáez Colque
+     * @param resultados[] arreglo usado para almacenar todos los resultados del sistema
+     * @param centro escogido por el usuario
+     * @param partido escogido por el usuario
+     * @return verdadero o falso dependiendo de si ya existe una trupleta con el centro y partido seleccionados
+     */  
     private static Boolean existeResultado(Resultado[] resultados, Centro centro, Partido partido) {
     
         for (Resultado resultado:resultados) {
@@ -166,7 +193,14 @@ public class MenuResultado {
         return false;
     
     }
-        
+    
+    /**
+     * Metodo que devuelve la cantidad de votos disponibles en un centro
+     * @author: Jose Luis Panadero, Gustavo Adolfo Hernández Quesada, Alvaro Francisco Hernáez Colque
+     * @param resultados[] arreglo usado para almacenar todos los resultados del sistema
+     * @param centro centro escogido por el usuario
+     * @return int es el número de votos disponibles del centro
+     */  
     private static int votosDisponibles(Resultado[] resultados, Centro centro) {
     
         int votosDisponibles = centro.getElectores();
@@ -183,7 +217,15 @@ public class MenuResultado {
         return votosDisponibles;
     
     }
-        
+    
+    /**
+     * Metodo que introduce la nueva tripleta en el arreglo donde se almacenan
+     * @author: Jose Luis Panadero, Gustavo Adolfo Hernández Quesada, Alvaro Francisco Hernáez Colque
+     * @param centros[] este parametro es el arreglo usado para almacenar todos los centos del sistema
+     * @param partidos[] este parametro es el arreglo usado para almacenar todos los partidos del sistema
+     * @param resultados[] este parametro es el arreglo usado para almacenar todos los resultados del sistema
+     * @return Resultado[] arreglo usado para almacenar todos los resultados del sistema
+     */   
     public static Resultado[] insertaTripleta(Centro[] centros, Partido[] partidos, Resultado[] resultados) {
     
         Resultado newResultados[] = resultados;
